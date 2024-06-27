936f2664010d4fd6a65bc4d6168be60f320b124f
aniketchauhan18 <workwithaniket18@gmail.com>
2024-06-27 13:52:54 +0530

    req to add rentedVehicle

--- frontend/src/pages/Rent.tsx
+++ frontend/src/pages/Rent.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate, useParams } from "react-router-dom";
 import { fetchUserData, fetchVehicle } from "@/fetchData";
-import VehicleImg from "../assets/hakon-sataoen-qyfco1nfMtg-unsplash.jpg"
+import VehicleImg from "../assets/hakon-sataoen-qyfco1nfMtg-unsplash.jpg";
 import { CiUser, CiMail } from "react-icons/ci";
 import { IoCallOutline } from "react-icons/io5";
 import { Label } from "@/components/ui/label";
@@ -16,13 +16,11 @@ interface RazorpayResponse {
   razorpay_signature: string;
 }
 
-
 function Rent(): JSX.Element {
   const { id } = useParams<string>();
-  const [startDate, setStartDate] = useState<string>("")
+  const [startDate, setStartDate] = useState<string>("");
   const [endDate, setEndDate] = useState<string>("");
   const navigateTo = useNavigate();
-
 
   const {
     data: vehicle,
@@ -51,11 +49,11 @@ function Rent(): JSX.Element {
   const checkout = async (amount: number | string) => {
     if (!startDate || !endDate) {
       alert("Please select the dates to rent the vehicle");
-      return
+      return;
     }
-    const days = calculateDaysBetweenDates(startDate, endDate)
-    const paymentAmount = Number(amount) * (days + 1)
-    console.log(paymentAmount)
+    const days = calculateDaysBetweenDates(startDate, endDate);
+    const paymentAmount = Number(amount) * (days + 1);
+    console.log(paymentAmount);
     try {
       const keyResponse = await fetch("http://localhost:3001/payments/key");
       const { key } = await keyResponse.json();
@@ -64,14 +62,14 @@ function Rent(): JSX.Element {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ amount:paymentAmount }),
+        body: JSON.stringify({ amount: paymentAmount }),
       });
       const { order } = await response.json();
-      
-      console.log(days)
+
+      console.log(days);
 
       const options = {
-        key: key, 
+        key: key,
         amount: paymentAmount, // Amount should be in currency subunits (e.g., paise for INR)
         currency: "INR",
         name: "Yana",
@@ -82,16 +80,20 @@ function Rent(): JSX.Element {
         prefill: {
           name: owner.username,
           email: owner.email,
-          contact: owner.number
+          contact: owner.number,
         },
         notes: {
-          address: "Razorpay Corporate Office"
+          address: "Razorpay Corporate Office",
         },
         theme: {
-          color: "#3399cc"
+          color: "#3399cc",
         },
         handler: async function (response: RazorpayResponse) {
-          if (!response.razorpay_payment_id || !response.razorpay_order_id || !response.razorpay_signature) {
+          if (
+            !response.razorpay_payment_id ||
+            !response.razorpay_order_id ||
+            !response.razorpay_signature
+          ) {
             console.error("Razorpay response is missing required properties.");
             return;
           }
@@ -99,60 +101,89 @@ function Rent(): JSX.Element {
           console.log("Payment ID: ", response.razorpay_payment_id);
           console.log("Order ID: ", response.razorpay_order_id);
           console.log("Signature: ", response.razorpay_signature);
-          const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = response;
-          const storePaymentResponse = await fetch(`http://localhost:3001/payments/store/${id}`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${localStorage.getItem("yana-token")}`,
+          const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+            response;
+          const storePaymentResponse = await fetch(
+            `http://localhost:3001/payments/store/${id}`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("yana-token")}`,
+              },
+              body: JSON.stringify({
+                userId: owner?._id,
+                amount: paymentAmount * 100,
+                razorpayPaymentId: razorpay_payment_id,
+                razorpayOrderId: razorpay_order_id,
+                razorpaySignature: razorpay_signature,
+                status: "Success",
+              }),
             },
-            body: JSON.stringify({
-              userId : owner?._id,
-              amount: paymentAmount * 100,
-              razorpayPaymentId: razorpay_payment_id,
-              razorpayOrderId: razorpay_order_id,
-              razorpaySignature: razorpay_signature,
-              status: "Success",
-            })
-          });
+          );
 
-          const paymentResponse = await storePaymentResponse.json()
-          const checkoutUrl = `/payment/checkout?paymentId=${razorpay_payment_id}&orderId=${razorpay_order_id}&signature=${razorpay_signature}&amount=${paymentAmount}`
+          const paymentResponse = await storePaymentResponse.json();
+          const checkoutUrl = `/payment/checkout?paymentId=${razorpay_payment_id}&orderId=${razorpay_order_id}&signature=${razorpay_signature}&amount=${paymentAmount}`;
           console.log(paymentResponse);
-          const updateVehicle = await fetch(`http://localhost:3001/vehicles/update/${id}`, {
+          const updateVehicle = await fetch(
+            `http://localhost:3001/vehicles/update/${id}`,
+            {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("yana-token")}`,
+              },
+              body: JSON.stringify({
+                isAvailable: "No",
+                startDate,
+                endDate,
+                bookedBy: localStorage.getItem("yana-user"),
+              }),
+            },
+          );
+          const updateVehicleData = await updateVehicle.json();
+          console.log(updateVehicleData);
+          const addrentedVehicle = await fetch(`http://localhost:3001/users/add/rented-vehicles/${localStorage.getItem("yana-user")}`, {
             method: "PUT",
             headers: {
               "Content-Type": "application/json",
               Authorization: `Bearer ${localStorage.getItem("yana-token")}`,
             },
-            body: JSON.stringify({ isAvailable: "No", startDate, endDate, bookedBy: localStorage.getItem("yana-user") }),
+            body: JSON.stringify({
+              rentedVehicles: [vehicle?._id]
+            })
           })
-          const updateVehicleData = await updateVehicle.json()
-          console.log(updateVehicleData);
-          navigateTo(checkoutUrl)
-        }
+          if (!addrentedVehicle.ok) {
+            alert("error while adding vehicle")
+          }
+          navigateTo(checkoutUrl);
+        },
       };
-      
+
       const rzp1 = new window.Razorpay(options);
       rzp1.open();
-
     } catch (err) {
       console.error("Error in checkout process:", err);
     }
   };
-
 
   return (
     <main className="p-6 text-zinc-700  flex items-center w-full mb-24">
       <div className="grid lg:grid-cols-2 space-y-10 w-full space-x-4">
         <div className="flex justify-center items-center flex-col w-full">
           <div className="flex justify-center items-center w-full flex-col space-y-3">
-            <img src={VehicleImg} className="max-w-xl rounded-xl w-full"/>
+            <img src={VehicleImg} className="max-w-xl rounded-xl w-full" />
             <div className="grid  p-3 w-full max-w-sm sm:max-w-xl rounded-md ">
-              <h1 className="text-3xl font-bold">{vehicle?.make}</h1>
-              <p className="text-xl text-zinc-500">{vehicle?.model} </p>
-              <p className="text-xl text-zinc-500">${vehicle?.price} / day</p>
-              <p className="text-xl text-zinc-500">Availablity : {vehicle?.isAvailable}</p>
+              <h1 className="text-3xl font-bold">
+                {vehicle?.make} / {vehicle?.model}
+              </h1>
+              <p className="text-lg text-zinc-500"></p>
+              <p className="text-lg text-zinc-4 00">
+                ${vehicle?.price} per day
+              </p>
+              <p className="text-xl text-zinc-500">
+                Availablity : {vehicle?.isAvailable}
+              </p>
             </div>
             <div className="grid border p-3 w-full max-w-sm sm:max-w-xl rounded-md bg-zinc-100/40">
               <h1 className="text-xl font-bold mb-3">Owner Information</h1>
@@ -184,11 +215,23 @@ function Rent(): JSX.Element {
             </div>
             <div>
               <Label htmlFor="start-date">Start Date</Label>
-              <Input type="date" id="start-date" className="max-w-[10rem]" value={startDate} onChange={(e) => setStartDate(e.target.value)}/>
+              <Input
+                type="date"
+                id="start-date"
+                className="max-w-[10rem]"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
             </div>
             <div>
               <Label htmlFor="end-date">End Date</Label>
-              <Input type="date" id="start-date" className="max-w-[10rem]" value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
+              <Input
+                type="date"
+                id="start-date"
+                className="max-w-[10rem]"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
             </div>
           </div>
           <div className="border-b" />
